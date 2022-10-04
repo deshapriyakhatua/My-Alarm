9d6c1f70d8673b9e712f6278e15806a3e183a012
deshapriya <deshapriyakhatua@gmail.com>
2022-10-04 19:38:01 +0530

    04 10 2022

--- app/src/main/java/com/example/myalarm/MainActivity.java
+++ app/src/main/java/com/example/myalarm/MainActivity.java
@@ -11,10 +11,11 @@ import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText hour,minute;
+    TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,59 +33,9 @@ public class MainActivity extends AppCompatActivity {
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.setStatusBarColor(Color.BLACK);
 
-        hour = findViewById(R.id.editTextHour);
-        minute = findViewById(R.id.editTextMinute);
+        timePicker = findViewById(R.id.timePicker);
 
-        hour.addTextChangedListener(new TextWatcher() {
-            String beforeChange;
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                beforeChange = charSequence.toString();
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(i2 > i1){
-                    int hours = Integer.parseInt(charSequence.toString());
-
-                    if(hours>23 || charSequence.length()>2){
-                        hour.setText(beforeChange);
-                        hour.setSelection(beforeChange.length()-1);
-                    }
-                }
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
-
-        minute.addTextChangedListener(new TextWatcher() {
-            String beforeChange;
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                beforeChange = charSequence.toString();
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-                if(i2 > i1){
-                    int minutes = Integer.parseInt(charSequence.toString());
-
-                    if(minutes>59 || charSequence.length()>2){
-                        minute.setText(beforeChange);
-                        minute.setSelection(beforeChange.length()-1);
-                    }
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
+        int hour = timePicker.getHour();
+        int minute = timePicker.getMinute();
     }
 }
