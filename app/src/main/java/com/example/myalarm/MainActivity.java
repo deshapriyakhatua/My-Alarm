b7a6b9066cb92775272480156a51f51d6530f079
deshapriya <deshapriyakhatua@gmail.com>
2022-10-06 21:08:46 +0530

    06 10 2022

--- app/src/main/java/com/example/myalarm/MainActivity.java
+++ app/src/main/java/com/example/myalarm/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.myalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +21,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // hiding action bar
         getSupportActionBar().hide();
+
+        // hiding status bar
+        WindowCompat.setDecorFitsSystemWindows(getWindow(),false);
+
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(Color.BLACK);
 
         hour = findViewById(R.id.editTextHour);
         minute = findViewById(R.id.editTextMinute);
