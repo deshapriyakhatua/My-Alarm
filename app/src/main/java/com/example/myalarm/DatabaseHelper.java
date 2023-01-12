c0ed81af3b654bf370e5ab3e9ddb47f0c2d9ac78
deshapriya <deshapriyakhatua@gmail.com>
2023-01-12 22:25:23 +0530

    12 01 2023

--- app/src/main/java/com/example/myalarm/DatabaseHelper.java
+++ app/src/main/java/com/example/myalarm/DatabaseHelper.java
@@ -1,9 +1,11 @@
 package com.example.myalarm;
 
 import android.app.DownloadManager;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -16,12 +18,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String TABLE_NAME = "table1";
     private static final String COLUMN_ID = "id";
     private static final String COLUMN_TITLE = "label";
-    private static final int COLUMN_HOUR = 12;
-    private static final int COLUMN_MINUTE = 10;
-    private static final boolean COLUMN_ACTIVE =  false;
+    private static final String COLUMN_HOUR = "hour";
+    private static final String COLUMN_MINUTE = "minute";
+    private static final String COLUMN_ACTIVE =  "active";
 
-    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+    public DatabaseHelper(@Nullable Context context) {
+
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
     }
 
     @Override
@@ -43,5 +47,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    void addData(String title, int hour, int minute, boolean active){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(COLUMN_TITLE,title);
+        contentValues.put(COLUMN_HOUR,hour);
+        contentValues.put(COLUMN_MINUTE,minute);
+        contentValues.put(COLUMN_ACTIVE,active);
+
+        long result = db.insert(TABLE_NAME,null,contentValues);
+
+        if(result == -1){
+            Toast.makeText(context, "Failed to upload", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "Uploaded successfully", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
